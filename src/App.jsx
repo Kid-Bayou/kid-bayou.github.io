effd123a70b73a9a382acf433a5ab6405942ed72
Kidist T. <88617617+Kid-Bayou@users.noreply.github.com>
2024-12-10 08:54:06 +0300

    Changed the basename/homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "./styles/layout.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/kid-bayou-">
+    <BrowserRouter basename="/kid-bayou.github.io">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
