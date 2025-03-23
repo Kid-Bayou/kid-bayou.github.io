a3e717020e71cf5fcd87b7ef37b05a7434ae164f
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2025-03-23 00:23:47 -0400

    More updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import "./styles/layout.css";
-import "./styles/sections.css";
 import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import Footer from "./components/sections/Footer";
 import Header from "./components/sections/Header";
+
+
+import "./App.css";
+import "./styles/layout.css";
+import "./styles/sections.css";
 
 function App() {
   return (
--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,16 +1,20 @@
+import linkedInLogo from "../../assets/icons/linkedin.svg";
+import emailLogo from "../../assets/icons/email.svg";
+import gitHubLogo from "../../assets/icons/github.svg";
+
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer-copyright">Copyright © 2024 kid-bayou</p>
       <div className="footer-icon-container">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/icons/github.svg" alt="GitHub" className="footer-icon" />
+        <a href="mailto:example@example.com">
+          <img src={emailLogo} alt="Email" className="footer-icon" />
         </a>
         <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/icons/linkedin.svg" alt="LinkedIn" className="footer-icon" />
+          <img src={linkedInLogo} alt="LinkedIn" className="footer-icon" />
         </a>
-        <a href="mailto:example@example.com">
-          <img src="/assets/icons/email.svg" alt="Email" className="footer-icon" />
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <img src={gitHubLogo} alt="GitHub" className="footer-icon" />
         </a>
       </div>
     </footer>
