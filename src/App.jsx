274da798fdb10ef0df349e1aff6153b5fefa1e0d
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2025-03-22 23:58:25 -0400

    Updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,18 @@ import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
+import Footer from "./components/sections/Footer";
+import Header from "./components/sections/Header";
 
 function App() {
-  const [activeSection, setActiveSection] = useState("home");
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = ["home", "about", "projects", "contact"];
-      const scrollPosition = window.scrollY + 100;
-      
-      for (let section of sections) {
-        const element = document.getElementById(section);
-        if (element && element.offsetTop <= scrollPosition) {
-          setActiveSection(section);
-        }
-      }
-    };
-    
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="site-wrapper">
-      <Header activeSection={activeSection} />
-      <main className="main">
-        <section id="home">
-          <Home />
-        </section>
-        <section id="about">
-          <About />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-        <section id="contact">
-          <Contact />
-        </section>
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </main>
       <Footer />
     </div>
