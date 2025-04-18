319cd7cb96d6a32b31e2140e7bba2e3db48b9dd2
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2025-04-17 23:58:48 -0400

    slight alterations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,8 @@
-import EmailIcon from '@mui/icons-material/Email';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Footer() {
   return (
     <footer className="footer">
       <p className="footer-copyright">Copyright © 2024 kid-bayou</p>
-      <div className="footer-icon-container">
-      <div style={{ display: 'flex', gap: '20px' }}>
-      <a href="mailto:your@email.com" target="_blank" rel="noopener noreferrer">
-        <EmailIcon fontSize="large" />
-      </a>
-      <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
-        <LinkedInIcon fontSize="large" />
-      </a>
-      <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
-        <GitHubIcon fontSize="large" />
-      </a>
-    </div>
-      </div>
     </footer>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,3 @@
-import EmailIcon from "@mui/icons-material/Email";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Home() {
   return (
@@ -9,29 +6,6 @@ function Home() {
         <h1>Home</h1>
         <p>Insert whatever here. What do you even put here???</p>
 
-        <div style={{ display: "flex", gap: "20px" }}>
-          <a
-            href="mailto:your@email.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <EmailIcon fontSize="large" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedInIcon fontSize="large" className="social-icon" />
-          </a>
-          <a
-            href="https://github.com/yourusername"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GitHubIcon fontSize="large" />
-          </a>
-        </div>
       </div>
     </section>
   );
